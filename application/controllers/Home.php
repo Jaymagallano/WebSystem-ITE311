65a7fdcc3449400c3259e9c02a7bc89ea99e07c3
Jaymagallano <jaymagallano2004@gmail.com>
2025-08-28 03:31:34 +0800

    Added Home controller, routes, and views for basic navigation

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
     
     public function index() {
-        $this->load->view('template');
+        $this->load->view('index');
+    }
+    
+    public function about() {
+        $this->load->view('about');
+    }
+    
+    public function contact() {
+        $this->load->view('contact');
     }
 }
 ?>
